978eb5b83430ad85586320d1bcf5564bf9f0d847
Saikumars26 <saikumarsheri01@gmail.com>
2025-04-11 00:27:37 -0500

    added done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [students, setStudents] = useState([]);
 
   useEffect(() => {
-    fetch('https://clouddevbackend-e9f7g2h3hdhrbpde.centralus-01.azurewebsites.net/api/students')
+    fetch('https://dbwebsite-fre9ehg0fycxendt.centralus-01.azurewebsites.net/api/students')
       .then(res => res.json())
       .then(data => setStudents(data))
       .catch(err => console.error('Error fetching students:', err));
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Student List</h1>
+      <h1>Student List for sheri</h1>
       <table border="1" cellPadding="10">
         <thead>
           <tr>
